c4cf8a7bec8f87ccf2bff67ea646c25bea45d056
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-06-24 22:07:55 +0200

    add bookingcalendar with name and email

--- index.php
+++ index.php
@@ -1,50 +1,70 @@
 <?php
     function build_calendar($month, $year) {
-        //Tableau contenant les noms de tous les jours de la semaine
-        $daysOfWeek = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
 
-        //Premier jour du mois qui est l'argument de cette fonction
+        $mysqli = new mysqli('localhost', 'root', 'root', 'bookingcalendar');
+        $stmt = $mysqli->prepare("select * from bookings where MONTH(date) = ? AND YEAR(date) = ?");
+        $stmt->bind_param('ss', $month, $year);
+        $bookings = array();
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+            if($result->num_rows>0){
+                while($row = $result->fetch_assoc()){
+                    $bookings[] = $row['date'];
+                }
+                $stmt->close();
+            }
+        }
+
+        // Create array containing abbreviations of days of week.
+        $daysOfWeek = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+
+        // What is the first day of the month in question?
         $firstDayOfMonth = mktime(0,0,0,$month,1,$year);
 
-        //Obtention du nombre de jours de ce mois 
-        $numberDays = date("t", $firstDayOfMonth);
+        // How many days does this month contain?
+        $numberDays = date('t',$firstDayOfMonth);
 
-        //Information sur le premier jour de ce mois
+        // Retrieve some information about the first day of the
+        // month in question.
         $dateComponents = getdate($firstDayOfMonth);
 
-        //Obtenir le nom de ce mois
+        // What is the name of the month in question?
         $monthName = $dateComponents['month'];
 
-        //Getting the index value 0-6 of the first day of this month
+        // What is the index value (0-6) of the first day of the
+        // month in question.
         $dayOfWeek = $dateComponents['wday'];
 
-        //Getting the current date
-        $dateToday = date('Y-m-d');
+        // Create the table tag opener and day headers
+     
+        $datetoday = date('Y-m-d');
 
-        //Now creating the HTML Table
         $calendar = "<table class='table table-bordered'>";
-        $calendar.= "<center><h2>$monthName $year</h2>";
-        
+        $calendar .= "<center><h2>$monthName $year</h2>";
         $calendar.= "<a class='btn btn-xs btn-primary' href='?month=".date('m', mktime(0, 0, 0, $month-1, 1, $year))."&year=".date('Y', mktime(0, 0, 0, $month-1, 1, $year))."'>Previous Month</a> ";
+        
+        $calendar.= " <a class='btn btn-xs btn-primary' href='?month=".date('m')."&year=".date('Y')."'>Current Month</a> ";
+        
         $calendar.= "<a class='btn btn-xs btn-primary' href='?month=".date('m', mktime(0, 0, 0, $month+1, 1, $year))."&year=".date('Y', mktime(0, 0, 0, $month+1, 1, $year))."'>Next Month</a></center><br>";
 
 
-        $calendar.= "<tr>";
+        $calendar .= "<tr>";
 
         //Creating the calendar headers 
         foreach($daysOfWeek as $day) {
-            $calendar.="<th class='header'>$day</th>";
+            $calendar .="<th class='header'>$day</th>";
         }
+
+        $currentDay = 1;
 
         $calendar.="</tr><tr>";
 
         //the variable $dayOfWeek will make sure that there must be only 7 columns on our table
         if($dayOfWeek > 0){
             for($k=0; $k<$dayOfWeek; $k++) {
-                $calendar.="<td></td>";
+                $calendar .="<td class='empty'></td>";
             }
         }
-        $currentDay = 1;
 
         $month = str_pad($month, 2, "0", STR_PAD_LEFT);
 
@@ -52,16 +72,22 @@
 
             if($dayOfWeek == 7) {
                 $dayOfWeek = 0;
-                $calendar.="</tr><tr>";
+                $calendar .="</tr><tr>";
             }
 
             $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
             $date = "$year-$month-$currentDayRel";
 
-            if($dateToday==$date){
-                $calendar.="<td class='today'><h4>$currentDay</h4>";
-            } else {
-                $calendar.="<td><h4>$currentDay</h4>";
+            $dayname = strtolower(date('l', strtotime($date)));
+            $eventNum = 0;
+            $today = $date==date('Y-m-d')? "today" : "";
+
+            if($date<date('Y-m-d')){
+                $calendar.="<td><h4>$currentDay</h4> <button class='btn btn-danger btn-xs'>N/A</button>";
+            }elseif(in_array($date, $bookings)){
+                $calendar.="<td class='$today'><h4>$currentDay</h4> <button class='btn btn-danger btn-xs'>Already Booked</button>";
+            }else{
+                $calendar.="<td class='$today'><h4>$currentDay</h4> <a href='book.php?date=".$date."' class='btn btn-success btn-xs'>Book</a>";
             }
 
             $calendar.="</td>";
@@ -70,14 +96,14 @@
             $dayOfWeek++;
         }
         if($dayOfWeek != 7) {
-            $remainingDays = 7-$dayOfWeek;
+            $remainingDays = 7 - $dayOfWeek;
             for($i=0; $i<$remainingDays; $i++) {
-                $calendar.= "<td></td>";
+                $calendar .= "<td class='empty'></td>";
             }
         }
 
-        $calendar.="</tr>";
-        $calendar.="</table>";
+        $calendar .= "</tr>";
+        $calendar .= "</table>";
 
         echo $calendar;
     }
@@ -96,17 +122,104 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
     <style>
-        table {
-            table-layout: fixed;
+        @media only screen and (max-width: 760px),
+        (min-device-width: 802px) and (max-device-width: 1020px) {
+
+            /* Force table to not be like tables anymore */
+            table, thead, tbody, th, td, tr {
+                display: block;
+
+            }
+            
+            
+
+            .empty {
+                display: none;
+            }
+
+            /* Hide table headers (but not display: none;, for accessibility) */
+            th {
+                position: absolute;
+                top: -9999px;
+                left: -9999px;
+            }
+
+            tr {
+                border: 1px solid #ccc;
+            }
+
+            td {
+                /* Behave  like a "row" */
+                border: none;
+                border-bottom: 1px solid #eee;
+                position: relative;
+                padding-left: 50%;
+            }
+
+
+
+            /*
+		Label the data
+		*/
+            td:nth-of-type(1):before {
+                content: "Sunday";
+            }
+            td:nth-of-type(2):before {
+                content: "Monday";
+            }
+            td:nth-of-type(3):before {
+                content: "Tuesday";
+            }
+            td:nth-of-type(4):before {
+                content: "Wednesday";
+            }
+            td:nth-of-type(5):before {
+                content: "Thursday";
+            }
+            td:nth-of-type(6):before {
+                content: "Friday";
+            }
+            td:nth-of-type(7):before {
+                content: "Saturday";
+            }
+
+
         }
 
-        td {
-            width: 33%;
+        /* Smartphones (portrait and landscape) ----------- */
+
+        @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+            body {
+                padding: 0;
+                margin: 0;
+            }
         }
 
-        .today {
-            background: yellow;
+        /* iPads (portrait and landscape) ----------- */
+
+        @media only screen and (min-device-width: 802px) and (max-device-width: 1020px) {
+            body {
+                width: 495px;
+            }
         }
+
+        @media (min-width:641px) {
+            table {
+                table-layout: fixed;
+            }
+            td {
+                width: 33%;
+            }
+        }
+        
+        .row{
+            margin-top: 20px;
+        }
+        
+        .today{
+            background:yellow;
+        }
+        
     </style>
 </head>
 
@@ -116,9 +229,14 @@
             <div class="col-md-12">
                 <?php
                     $dateComponents = getdate();
-                    $month = $dateComponents['mon'];
-                    $year = $dateComponents['year'];
-                    echo build_calendar($month, $year);
+                    if(isset($_GET['month']) && isset($_GET['year'])){
+                        $month = $_GET['month']; 			     
+                        $year = $_GET['year'];
+                    }else{
+                        $month = $dateComponents['mon']; 			     
+                        $year = $dateComponents['year'];
+                    }
+                    echo build_calendar($month,$year);
                 ?>
             </div>
         </div>
